ce95bd75a28e0063be921a47400acc77d58416aa
prashantrajputRepo <prashantrajput0912@gmail.com>
2026-05-03 17:22:53 +0530

    fix skills undefined issue 2.8.3

--- client/src/pages/Notes.jsx
+++ client/src/pages/Notes.jsx
@@ -130,7 +130,7 @@ const Notes = () => {
                                    </a>
                               )}
 
-                              <p className="whitespace-pre-wrap break-words font-mono text-sm mt-2">
+                              <p className="whitespace-pre-wrap break-words font-mono text-sm mt-2 max-h-96 overflow-y-auto">
                                    {note.content}
                               </p>
 
